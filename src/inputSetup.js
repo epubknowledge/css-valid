f88b2de4d063ff3f402ea06ce72c09a131443a6c
codingChewie <codingChewie@gmail.com>
2021-08-05 18:43:30 -0500

    rewrote for only files

--- src/inputSetup.js
+++ src/inputSetup.js
@@ -1,16 +1,4 @@
-path = require('path')
-
-const absPath = require('./absPath')
-
 module.exports = i => {
-  let obj = {}
   let cssArray = []
-  if (Array.isArray(i) === true) {
-    cssArray = i
-  } else {
-    cssArray = i.split(',')
-  }
-  obj.dir = path.dirname(absPath(cssArray[0]))
-  obj.files = cssArray
-  return obj
+  return Array.isArray(i) === true ? (cssArray = i) : (cssArray = i.split(','))
 }
