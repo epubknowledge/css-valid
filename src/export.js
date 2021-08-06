48e5873e4e866cfa9adc71f1a58ddebcc7939892
codingChewie <codingChewie@gmail.com>
2021-08-06 14:55:04 -0500

    added export to XML

---
+++ src/export.js
@@ -0,0 +1,21 @@
+const fs = require('fs-extra')
+const chalk = require('chalk')
+
+module.exports = obj => {
+  obj.input.files.map(item => {
+    const passed = item.errors === 0 && item.warnings === 0 ? true : false
+    const filename = passed === true ? chalk.green(item.file) : chalk.red(item.file)
+    const erCount = item.errors === 0 ? item.errors : chalk.red(item.errors)
+    const waCount = item.warnings === 0 ? item.warnings : chalk.red(item.warnings)
+    console.log(
+      chalk.white('File:'),
+      filename,
+      chalk.white('Errors:'),
+      erCount,
+      chalk.white('Warnings:'),
+      waCount,
+    )
+    if (passed !== true) fs.outputFileSync(`${obj.output.dir}/${item.file}.xml`, item.data)
+  })
+  console.log(chalk.green('\nCompleted'))
+}
