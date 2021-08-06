d240b5c4b3d0dd2df54ffd4d7a60d29475f3f3c8
codingChewie <codingChewie@gmail.com>
2021-08-06 09:34:37 -0500

    try/catch added

--- src/isDir.js
+++ src/isDir.js
@@ -1,7 +1,13 @@
 fs = require('fs')
 path = require('path')
+const chalk = require('chalk')
 
 module.exports = dir => {
-  const absolute = path.isAbsolute(dir) ? dir : path.join(process.cwd(), dir)
-  return fs.lstatSync(absolute).isDirectory() === true ? absolute : false
+  try {
+    const absolute = path.isAbsolute(dir) ? dir : path.join(process.cwd(), dir)
+    return fs.lstatSync(absolute).isDirectory() === true ? absolute : false
+  } catch {
+    console.log(chalk.red('Error:'), chalk.white(`Not a valid directory`))
+    process.exit(1)
+  }
 }
